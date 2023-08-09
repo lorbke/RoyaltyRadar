3c89fc8bf14447769fc7ba1468a60efe2e062556
lorbke <lorbke@student.42heilbronn.de>
2023-08-10 01:04:00 +0200

    remove comments

--- backend/contracts/RoyaltyDistributor.sol
+++ backend/contracts/RoyaltyDistributor.sol
@@ -3,46 +3,6 @@ pragma solidity ^0.8.0;
 
 pragma solidity ^0.8.0;
 
-// contract RoyaltyDistributor {
-//     struct Pool {
-//         uint256 total_received;
-//     }
-
-//     Pool[] public artist_pools;
-//     mapping (address => uint256) public artist_pool_index;
-
-//     function registerArtist() external {
-//         // Ensure the artist is not already registered.
-//         require(artist_pool_index[msg.sender] == 0, "Artist already registered");
-
-//         artist_pools.push(Pool(0));  // Initialize total_received to 0
-//         artist_pool_index[msg.sender] = artist_pools.length; // Index starts at 1, 0 is reserved for "not registered"
-//     }
-
-//     function getArtistPool(address artist) external view returns (Pool memory) {
-//         uint256 index = artist_pool_index[artist];
-//         require(index > 0, "Artist not registered");
-//         return artist_pools[index - 1];
-//     }
-
-//     function giveRoyalties(address artist) external payable {
-//         artist_pools[artist_pool_index[artist]].total_received += msg.value;
-//     }
-
-//     function buyRoyaltyRights(address artist, uint256 percentage) external {
-//         // artist_pools[artist_pool_index[artist]].stakes[msg.sender] += percentage;
-//     }
-
-//     function withdrawRoyalties() external {
-//         uint256 total_stake = 0;
-//         // for (uint256 i = 1; i < artist_pools.length; i++) {
-//         //     total_stake += artist_pools[i].stakes[msg.sender];
-//         // }
-//         // transfer to msg.sender
-//         payable(msg.sender).transfer(total_stake);
-//     }
-// }
-
 contract RoyaltyDistributor {
     struct Artist {
         address addr;
