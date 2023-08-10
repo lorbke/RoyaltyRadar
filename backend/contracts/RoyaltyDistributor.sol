8202a4e34dd87cfb414fe42e680826119d4c22ea
lorbke <lorbke@student.42heilbronn.de>
2023-08-10 23:07:02 +0200

    add events'

--- backend/contracts/RoyaltyDistributor.sol
+++ backend/contracts/RoyaltyDistributor.sol
@@ -13,6 +13,11 @@ contract RoyaltyDistributor {
     mapping (address => uint256) public artist_index;
     mapping (address => mapping (address => uint8)) public stakes;
 
+    event ArtistRegistered(address indexed _from, uint8 _shares, uint256 _share_price);
+    event RoyaltiesGiven(address indexed _from, address indexed _to, uint256 _value);
+    event RoyaltyRightsBought(address indexed _from, address indexed _to, uint256 _value, uint8 _shares);
+    event RoyaltiesWithdrawn(address indexed _from, uint256 _value);
+
     constructor() {
         Artist memory zero_artist = Artist(address(0), 0, 0, 0);
         artists.push(zero_artist);
@@ -20,15 +25,20 @@ contract RoyaltyDistributor {
 
     function registerArtist(uint8 shares_for_sale, uint256 share_price) external {
         require(artist_index[msg.sender] == 0, "Artist already registered");
+
         artist_index[msg.sender] = artists.length;
         Artist memory temp_artist = Artist(msg.sender, 0, shares_for_sale, share_price);
         artists.push(temp_artist);
         stakes[msg.sender][msg.sender] = 100;
+
+        emit ArtistRegistered(msg.sender, shares_for_sale, share_price);
     }
 
     function giveRoyalties(address artist) external payable {
         require (artist_index[artist] > 0, "Artist must be registered");
         artists[artist_index[artist]].total_received += msg.value;
+
+        emit RoyaltiesGiven(msg.sender, artist, msg.value);
     }
 
     function buyRoyaltyRights(address artist) external payable {
@@ -36,9 +46,12 @@ contract RoyaltyDistributor {
         require (artist_index[artist] > 0, "Artist must be registered");
         uint8 shares = (uint8)(msg.value / artists[artist_index[artist]].share_price);
         require (shares <= artists[artist_index[artist]].shares_for_sale, "Not enough shares available");
+
         stakes[artist][msg.sender] += shares;
         stakes[artist][artist] -= shares;
         artists[artist_index[artist]].shares_for_sale -= shares;
+
+        emit RoyaltyRightsBought(msg.sender, artist, msg.value, shares);
     }
 
     function getEarnedRoyalties() external view returns (uint256){
@@ -65,7 +78,10 @@ contract RoyaltyDistributor {
                 stakes[artist][msg.sender] = 0;
             }
         }
+
         require(total_stake > 0, "No royalties to withdraw");
         payable(msg.sender).transfer(total_stake);
+
+        emit RoyaltiesWithdrawn(msg.sender, total_stake);
     }
 }
